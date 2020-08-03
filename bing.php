b9ed607f77aaa782a2e0f30fd3eb09dc55879b8e
cuuuuuirz <cuuuuuirz@163.com>
2020-08-03 11:27:48 +0800

    fix

--- bing.php
+++ bing.php
@@ -1,6 +1,9 @@
 <?php
 
 $idx = $_POST['idx'];
+if(!isset($idx)) {
+	header("Location: /");
+}
 $url = "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=$idx&n=1&mkt=zh-CN";
 $data = json_decode(file_get_contents($url), true);
 $res = $data['images'][0];
